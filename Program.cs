b401465c463de906f09c347673dd7db9908664da
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-10-29 08:53:25 -0300

    Revert "init"
    This reverts commit c84633d7c7ad161dcf04e31e9a4d9a274dff663e.

--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using CG;
-
-// Aqui começa o programa. É criada uma instância da classe Game, representando
-//a janela. Em seguida, é chamada a função Run, que vai executar o loop de jogo
-//até que a janela seja fechada.
-//É dentro da classe Game que toda a lógica de jogo ficará situada.
-Game game = new Game("Meu Jogo", 800, 600);
-game.Run();
