4d5cc248aac9d5f27b4894883a45b18d24350f06
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-11-15 00:55:46 -0300

    detect collision with player

--- GameManager.cs
+++ GameManager.cs
@@ -44,7 +44,27 @@ namespace CG
             foreach (var obstacle in obstacles)
             {
                 obstacle.transform.position.Z += 4f * delta;
+
+                // 0.5 é o raio do collider do jogador
+                if (CheckCollision(playerTransform, obstacle.transform, 0.5f))
+                {
+                    Console.WriteLine("obstacle transform é igual ao player transform");
+                }               
             }
+        }
+        
+        private static bool CheckCollision(Transform collider1, Transform collider2, float offset)
+        {
+            // processing
+            // ship.getX() >= powerup.getX() - 35 &&
+            // ship.getX() <= powerup.getX() + 35 &&
+            // ship.getY() >= powerup.getY() - 35 &&
+            // ship.getY() <= powerup.getY() + 35)
+
+            return collider1.position.X >= collider2.position.X - offset &&
+                collider1.position.X <= collider2.position.X + offset &&
+                collider1.position.Z >= collider2.position.Z - offset &&
+                collider1.position.Z <= collider2.position.Z + offset;
         }
 
         private static void ResetToStartPosition()
